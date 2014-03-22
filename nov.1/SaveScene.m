cd3ddcb4f4b4ae2f004af613bc4358ee0a8f94e2
eccyun <rt.notes0201@gmail.com>
2014-03-22 16:53:47 +0900

    画面作成完了

--- nov.1/SaveScene.m
+++ nov.1/SaveScene.m
@@ -26,14 +26,20 @@
 // on "init" you need to initialize your instance
 -(id) init{
     if((self=[super init])){
+        self.isTouchEnabled = YES;
     }
     
 	return self;
 }
 
+// on "dealloc" you need to release all your retained objects
+- (void) dealloc{
+	[super dealloc];
+}
+
 -(void) onEnter{
 	[super onEnter];
-    
+
     CGSize size = [[CCDirector sharedDirector] winSize];
 
     CCSprite *background = [[CCSprite alloc] initWithFile:@"save_background.png"];
@@ -43,7 +49,7 @@
     CCSprite *save_1 = [[CCSprite alloc] initWithFile:@"save_1.png"];
     save_1.position  = ccp(size.width/2 - 142.f, size.height/2 + 50.f);
     [self addChild:save_1];
-    
+
     CCSprite *save_2 = [[CCSprite alloc] initWithFile:@"save_2.png"];
     save_2.position  = ccp(size.width/2, size.height/2 + 50.f);
     [self addChild:save_2];
@@ -63,6 +69,56 @@
     CCSprite *save_6 = [[CCSprite alloc] initWithFile:@"save_6.png"];
     save_6.position  = ccp(size.width/2 + 142.f, size.height/2 - 62.f);
     [self addChild:save_6];
+
+    // 左上のウィンドウ
+    CCSprite *load = [[CCSprite alloc] initWithFile:@"load_on.png"];
+    load.position  = ccp(size.width/2 - 100.f, size.height/2 + 140.f);
+    [self addChild:load];
+
+    CCSprite *save = [[CCSprite alloc] initWithFile:@"save_off.png"];
+    save.position  = ccp(size.width/2 - 182.f, size.height/2 + 139.f);
+    [self addChild:save];
+
+
+    CCSprite *back_btn = [[CCSprite alloc] initWithFile:@"save_back.png"];
+    back_btn.position  = ccp(size.width/2 + 182.f, size.height/2 - 142.f);
+    [self addChild:back_btn];
+    back_btn.tag = 1;
+
 }
+
+#pragma mark GameKit delegate
+-(void) registerWithTouchDispatcher{
+    CCDirector *director = [CCDirector sharedDirector];
+    [[director touchDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
+}
+
+-(BOOL) ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    CGPoint location = [touch locationInView:[touch view]];
+	location         = [[CCDirector sharedDirector] convertToGL:location];
+
+    CCSprite *back_btn = (CCSprite *)[self getChildByTag:1];
+    if(location.x > back_btn.position.x-(back_btn.contentSize.width/2)&&
+       location.x < back_btn.position.x+(back_btn.contentSize.width/2)&&
+       location.y > back_btn.position.y-(back_btn.contentSize.height/2)&&
+       location.y < back_btn.position.y+(back_btn.contentSize.height/2)){
+        [[CCDirector sharedDirector] popScene];
+    }
+
+    return YES;
+}
+
+-(void) ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+}
+
+-(void) ccTouchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+}
+
+-(void) ccTouchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+}
+
 
 @end
