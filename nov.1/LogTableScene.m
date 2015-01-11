85d319ce424bdc044a9d17f74ea49720e76d8be9
eccyun <rt.notes0201@gmail.com>
2015-01-11 22:45:39 +0900

    テキストログ機能を実装

--- nov.1/LogTableScene.m
+++ nov.1/LogTableScene.m
@@ -34,6 +34,7 @@
 
         CCSprite *back_btn = [[CCSprite alloc] initWithFile:@"save_back.png"];
         back_btn.position  = ccp(size.width-((back_btn.contentSize.width/2.f)+20.f), size.height/2 - 142.f);
+        back_btn.tag       = 1;
         [self addChild:back_btn];
 
         CCSprite *caption = [[CCSprite alloc] initWithFile:@"log-caption.png"];
@@ -49,12 +50,14 @@
         self.tableView.layer.borderColor  = [UIColor clearColor].CGColor;
         self.tableView.layer.borderWidth  = 0;
         self.tableView.backgroundColor    = [UIColor clearColor];
-        self.tableView.separatorColor     = [UIColor clearColor];
+        self.tableView.separatorColor     = [UIColor whiteColor];
         self.tableView.allowsSelection    = NO;
         self.tableView.alpha              = 1.f;
         self.tableView.dataSource         = self;
         self.tableView.delegate           = self;
         [[[CCDirector sharedDirector] view] addSubview:self.tableView];
+        
+        [self.tableView setContentOffset:CGPointMake(0, self.tableView.contentSize.height - self.tableView.frame.size.height) animated:NO];
     }
     
     return self;
@@ -72,7 +75,21 @@
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 70.f;
+    NSUserDefaults *ud         = [NSUserDefaults standardUserDefaults];
+    NSArray        *log_array  = [ud objectForKey:@"log_array"];
+    NSDictionary   *dictionary = [log_array objectAtIndex:indexPath.row];
+
+    UILabel *name_label = [[UILabel alloc] init];
+    UILabel *text_label = [[UILabel alloc] init];
+    name_label.text = [dictionary objectForKey:@"name"];
+    text_label.text = [dictionary objectForKey:@"message"];
+    name_label.numberOfLines   = 0;
+    text_label.numberOfLines   = 0;
+
+    [name_label sizeToFit];
+    [text_label sizeToFit];
+
+    return name_label.frame.size.height+text_label.frame.size.height+30.f;
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -89,22 +106,39 @@
 
     static NSString *CellIdentifier = @"CellCustom";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    UILabel *label;
+    UILabel         *name_label, *text_label;
     
     if (cell == nil){
-        cell            = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-        label           = [[UILabel alloc] initWithFrame:CGRectMake(30.f, 10.f, size.width-60.f, 50.f)];
-        label.tag       = 1;
-        label.textColor = [UIColor whiteColor];
-        [cell.contentView addSubview:label];
+        cell                 = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        name_label           = [[UILabel alloc] init];
+        name_label.tag       = 1;
+        name_label.textColor = [UIColor whiteColor];
+        name_label.font      = [UIFont fontWithName:@"HiraKakuProN-W6" size:12];
+        [cell.contentView addSubview:name_label];
+        
+        text_label           = [[UILabel alloc] init];
+        text_label.tag       = 2;
+        text_label.textColor = [UIColor whiteColor];
+        text_label.font      = [UIFont fontWithName:@"HiraKakuProN-W6" size:12];
+        [cell.contentView addSubview:text_label];
     }else{
-        label = (UILabel *)[cell.contentView viewWithTag:1];
+        name_label = (UILabel *)[cell.contentView viewWithTag:1];
+        text_label = (UILabel *)[cell.contentView viewWithTag:2];
     }
 
-    label.font            = [UIFont fontWithName:@"HiraKakuProN-W6" size:12];
-    label.text            = [dictionary objectForKey:@"message"];
+    name_label.text            = [dictionary objectForKey:@"name"];
+    name_label.backgroundColor = [UIColor clearColor];
+    name_label.numberOfLines   = 0;
+    text_label.text            = [dictionary objectForKey:@"message"];
+    text_label.backgroundColor = [UIColor clearColor];
+    text_label.numberOfLines   = 0;
 
-//label.backgroundColor = [UIColor redColor];
+    [name_label sizeToFit];
+    [text_label sizeToFit];
+
+    name_label.frame = CGRectMake(30.f, 10.f, size.width-60.f, name_label.frame.size.height);
+    text_label.frame = CGRectMake(30.f, 20.f+name_label.frame.size.height, size.width-60.f, text_label.frame.size.height);
+
     return cell;
 }
 
@@ -113,10 +147,39 @@
     [super dealloc];
 }
 
-#pragma mark GameKit delegate
 -(void) registerWithTouchDispatcher{
     CCDirector *director = [CCDirector sharedDirector];
     [[director touchDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
+}
+
+-(BOOL) ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    CGPoint location = [touch locationInView:[touch view]];
+    location         = [[CCDirector sharedDirector] convertToGL:location];
+
+    CCSprite       *back_btn = (CCSprite *)[self getChildByTag:1];
+
+    if(location.x > back_btn.position.x-(back_btn.contentSize.width/2)&&
+       location.x < back_btn.position.x+(back_btn.contentSize.width/2)&&
+       location.y > back_btn.position.y-(back_btn.contentSize.height/2)&&
+       location.y < back_btn.position.y+(back_btn.contentSize.height/2)){
+        [self.tableView removeFromSuperview];
+        [[CCDirector sharedDirector] popScene];
+        return YES;
+    }
+    
+    return YES;
+}
+
+-(void) ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+}
+
+-(void) ccTouchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+{
+}
+
+-(void) ccTouchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
 }
 
 @end
